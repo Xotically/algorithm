cab8a794da98b1a39e5dba2cbbecac15b60484de
joenali <joenali@163.com>
2013-07-14 08:16:52 +0800

    tree

---
+++ BTree.php
@@ -0,0 +1,9 @@
+<?php
+set_time_limit(0);
+
+class BTree {
+
+    public function creatTree() {
+
+    }
+}
--- tree.php
+++ tree.php
@@ -1,2 +1,38 @@
 <?php
 set_time_limit(0);
+
+// 节点
+class NODE {
+    public $data = null;
+    public $left = null;
+    public $right = null;
+}
+
+
+class Tree {
+    public $root = null;
+    private $data = array();
+
+    public function __CONSTRUCT($data) {
+        $this->data = $data;
+    }
+
+    public function creatTree() {
+        while ($this->data) {
+            $value = array_pop($this->data);
+            insertNode($value);
+
+        }
+    }
+
+
+    public function insertNode($data) {
+        $node = new NODE;
+        $node->data = $data;
+        return $node;
+    }
+
+    public function deleteNode() {
+
+    }
+}
