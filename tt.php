497e6d26ab91213c704654c3285cfa3f479cd2a0
joenali <joenali@163.com>
2013-07-25 20:26:27 +0800

    test

---
+++ tt.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 二叉树实现
+ * @author joe
+ * @version 1.0
+ * @date 2013-06-25
+ */
+
+set_time_limit(0);
+
+// 节点
+class NODE {
+    public $data = null;
+    public $left = null;
+    public $right = null;
+}
+
+
+class Tree {
+    public $root = null;
+    private $data = array();
+
+    public function __CONSTRUCT($data) {
+        $this->data = $data;
+    }
+
+    public function creatTree() {
+        while ($this->data) {
+            $value = array_pop($this->data);
+            insertNode($value);
+
+        }
+    }
+
+    public function insertNode($data) {
+        $node = new NODE;
+        $node->data = $data;
+        return $node;
+    }
+
+    public function deleteNode() {
+
+    }
+}
